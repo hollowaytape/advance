d1b645785a7d86fc470a4ac48eb49771d7ad24d5
Max Silbiger <tsmaxweliot@gmail.com>
2014-03-27 11:01:36 -0400

    Gets the values correctly, error must be in autoincrementing primary key being None for everything.

--- populate.py
+++ populate.py
@@ -21,41 +21,48 @@ def mk_int(s):
 
 for table in address_tables:
     with open(table, 'rb') as f:
-        reader = csv.reader(f)
-        reader.next()
-        for row in reader:
-            first = row[0]
-            last = row[1]
-            address = row[2]
-            po_box = row[3]
-            rural_box = row[4]
-            city = row[5]
-            state = row[6]
-            zip = row[7]
-            phone = row[10]
-            email = NULL
-            try:
-                end_date = datetime.datetime.strptime(row[9], "%y-%m-%d")
-            except ValueError:
-                end_date = 'NULL'
-            try:
-                # This can't currently handle a '' value, the provenance of which is unknown...
-                start_date = end_date - datetime.timedelta(days=(mk_int(row[8]) / 12) * 365.24)
-            except (sqlite3.InterfaceError, TypeError, ValueError): # Possible errors: multiply by a string, multiply by a blank.
-                start_date = datetime.datetime.today()
+        plotlist = csv.reader(f, delimiter=',')
+        for i, line in enumerate(plotlist):
+                    first = line[0]
+                    last = line[1]
+                    address = line[2]
+                    po_box = line[3]
+                    rural_box = line[4]
+                    city = line[5]
+                    state = line[6]
+                    zip = line[7]
+                    phone = line[10]
+                    email = 'NULL'
+            
+            # Handling dates.
+                    if line[9] == '':
+                            end_date = 'NULL'
+                    else:
+                        try:
+                            end_date = datetime.datetime.strptime(line[9], "%y-%m-%d").date()
+                        except ValueError:
+                            end_date = 'NULL'
+                    if line[8] == 'ex' or line[8] == '':
+                        start_date = 'NULL'
+                    else:
+                        try:
+                            start_date = end_date - datetime.timedelta(days=(mk_int(line[8]) / 12) * 365.24)
+                        except (sqlite3.InterfaceError, TypeError, ValueError): # Possible errors: multiply by a string, multiply by a blank.
+                            start_date = 'NULL'
                 
-            start_date = datetime.datetime.today()
-            sort_code = row[11]
-            walk_sequence = row[12]
-            city_code = row[13]
-            zone = row[14]
-            level = row[15]
-            # renewal_date = row[16]
-            advance = True
-            clipper = False
+                    sort_code = line[11]
+                    walk_sequence = line[12]
+                    city_code = line[13]
+                    zone = line[14]
+                    level = line[15]
+                    # renewal_date = line[16]
+                    advance = True
+                    clipper = False
+                    
             
-            record = (first, last, address, po_box, rural_box, city, state, zip, start_date, end_date, phone, email, sort_code, walk_sequence, city_code, zone, level, advance, clipper)
+                    record = (first, last, address, po_box, rural_box, city, state, zip, start_date, end_date, phone, email, sort_code, walk_sequence, city_code, zone, level, advance, clipper)
             
-            c.execute("INSERT INTO subscriptions VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NULL)", record)
+                    c.execute("INSERT INTO subscriptions VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 1)", record)
+            
 conn.commit()
 conn.close()
