eaca06b99861d5d316a1ed71c6106de7868a7a9f
Max Silbiger <tsmaxweliot@gmail.com>
2014-04-03 10:13:49 -0400

    Adding loose files

---
+++ setup.py
@@ -0,0 +1,45 @@
+
+#
+# Default setup file for a Camelot application
+#
+# To build a windows installer, execute this file with :
+#
+#     python setup.py egg_info bdist_cloud wininst_cloud
+#
+# Running from the Python SDK command line
+#
+
+import datetime
+import logging
+
+from setuptools import setup, find_packages
+
+logging.basicConfig( level=logging.INFO )
+
+setup(
+    name = 'Subscriptions',
+    version = '1.0',
+    author = 'Max Silbiger',
+    url = 'github.com/hollowaytape',
+    include_package_data = True,
+    packages = find_packages(),
+    py_modules = ['settings', 'main'],
+    entry_points = {'gui_scripts':[
+                     'main = main:start_application',
+                    ],},
+    options = {
+        'bdist_cloud':{'revision':'0',
+                       'branch':'master',
+                       'uuid':'48d12133-9ac0-47da-8e70-e04190a36c2c',
+                       'update_before_launch':False,
+                       'default_entry_point':('gui_scripts','main'),
+                       'changes':[],
+                       'timestamp':datetime.datetime.now(),
+                       },
+        'wininst_cloud':{ 'excludes':'excludes.txt',
+                          'uuid':'48d12133-9ac0-47da-8e70-e04190a36c2c', },
+    }, 
+
+  )
+
+    
